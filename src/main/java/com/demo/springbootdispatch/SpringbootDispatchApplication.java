b3197d575c709145b6621425a857a58487207f7b
iu007 <84652455@qq.com>
2022-12-01 22:42:49 +0800

    init

---
+++ src/main/java/com/demo/springbootdispatch/SpringbootDispatchApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springbootdispatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDispatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDispatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/springbootdispatch/SpringbootDispatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springbootdispatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDispatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
